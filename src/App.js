effeea4e1a9a0ed8315c2374a0431dba017e7529
Nikoleta Stoeva <nikoletastoeva@abv.bg>
2021-12-08 10:23:41 +0200

    Revert "added a 'getAll' function"
    This reverts commit 0cc7e61a749f4241d82f35c3e9820b3784030e33.

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
-import { Button, Navbar, Container, Nav, Carousel } from "react-bootstrap";
-import Home from "./components/Home";
-import Header from "./components/Header";
-import {Route} from 'react-router-dom'
-import About from "./components/AboutAs";
-import Footer from "./components/Footer";
-import Home1 from "./components/Home1";
-import Register from "./components/auth/Register";
-import Login from "./components/auth/Login";
-import Create from "./components/Create";
-import Details from "./components/Details";
-import CustomProducts from "./components/CustomProducts";
-
-
+import logo from './logo.svg';
+import './App.css';
 
 function App() {
   return (
-<>
- <Header/>
- <Route path='/' exact component={Home1}/>
- <Route path='/about-as' exact component={About}/>
- <Route path='/register' exact component={Register}/>
- <Route path='/login' exact component={Login}/>
- <Route path='/create' exact component={Create}/>
- <Route path='/details/:productId' exact component={Details}/>
- <Route path='/custom-products' exact component={CustomProducts}/>
- <Footer/>
-
-</>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
 }
 
--- src/services/itemsService.js
+++
@@ -1,8 +0,0 @@
-export function getAll() {
-    return fetch('http://localhost:3030/jsonstore/our-products')
-       .then(res => res.json())
- }
- export function getOne(id) {
-    return fetch(`http://localhost:3030/jsonstore/our-products/${id}`)
-       .then(res => res.json())
- }
