f1b15782d916ee5baf45838559b19fe452965b2d
Nikoleta Stoeva <nikoletastoeva@abv.bg>
2021-12-14 15:48:16 +0200

    add navigation for guest and another for user

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,9 @@
-import { Button, Navbar, Container, Nav, Carouse} from "react-bootstrap";
+
+import { Button, Navbar, Container, Nav } from "react-bootstrap";
 import NavbarCollapse from "react-bootstrap/esm/NavbarCollapse";
 import NavbarToggle from "react-bootstrap/esm/NavbarToggle";
 import { Link } from "react-router-dom";
+import { useAuthContext } from "../contexts/AuthContext";
 
 
 const Header = () => {
@@ -18,12 +20,26 @@ const Header = () => {
         padding: '24px 13px 24px 13px',
         backgroundColor: '#F2F3F3',
     }
+    let { user } = useAuthContext()
+
+    let guestNav = (
+        <Nav>
+            <Nav.Link to="/register" as={Link}>Register</Nav.Link>
+            <Nav.Link to="/login" as={Link}>Login</Nav.Link>
+        </Nav>)
+
+    let userNav = (
+        <Nav>
+            <Nav.Link to="/logout" as={Link}>Logout</Nav.Link>
+        </Nav>
+
+    )
 
     return (
-        <Navbar sticky="top" style={{backgroundColor:"#f5f0ec", borderBottom: "1px", borderBottomColor: "#e6dbd1", borderBottomStyle:"solid"}}>
+        <Navbar sticky="top" style={{ backgroundColor: "#f5f0ec", borderBottom: "1px", borderBottomColor: "#e6dbd1", borderBottomStyle: "solid" }}>
             <Container>
                 <Navbar.Brand to="/" as={Link}><img
-                    src="StudioNovo.png"
+                    src="/StudioNovo.png"
                 /></Navbar.Brand>
                 <NavbarToggle aria-controls='responsive-navbar-nav' />
                 <NavbarCollapse id='responsive-navbar-nav'>
@@ -31,14 +47,22 @@ const Header = () => {
                         <Nav.Link to="/" as={Link}>Home</Nav.Link>
                         <Nav.Link to="/about-as" as={Link}>About as</Nav.Link>
                         <Nav.Link to="/custom-products" as={Link}>Custom products</Nav.Link>
-                        <Nav.Link to="/my-love" as={Link}>My love</Nav.Link>
-                        
+                        {user.email ? <Nav.Link to="/my-love" as={Link}>My love</Nav.Link> : ""}
+
                     </Nav>
-                    <Button to="/create" as={Link} style={styleButton}>Create product</Button>
-                    <Nav>
-                        <Nav.Link to="/register" as={Link}>Register</Nav.Link>
-                        <Nav.Link to="/login" as={Link}>Login</Nav.Link>
-                    </Nav>
+
+
+                    {user.email
+                        ? <Button to="/create" as={Link} style={styleButton}>Create product</Button>
+                        : ""
+                    }
+                    
+                    {user.email
+                        ? userNav
+                        : guestNav
+                    }
+
+
                 </NavbarCollapse>
             </Container>
         </Navbar>
