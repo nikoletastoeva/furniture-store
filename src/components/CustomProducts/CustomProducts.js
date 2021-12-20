946a483eb94fbf7f4fc4de35ba3c93ff4ecabe74
Nikoleta Stoeva <nikoletastoeva@abv.bg>
2021-12-20 16:19:47 +0200

    function show products or haven't products yet

--- src/components/CustomProducts/CustomProducts.js
+++ src/components/CustomProducts/CustomProducts.js
@@ -1,6 +1,6 @@
 
 import { useEffect, useState } from "react/cjs/react.development"
-import * as itemsService from '../../services/itemsService' 
+import * as itemsService from '../../services/itemsService'
 import ProductCardCustom from "../ProductCardCustom/ProductCardCustom"
 import "./CustomProducts.css"
 
@@ -19,13 +19,18 @@ const CustomProducts = () => {
 
     return (
         <section>
-                <h2>CUSTOM PRODUCTS</h2>
-                <div className="container mydiv">
-                    <div className="row row-cols-3">
-                    {products.map(x => <ProductCardCustom key={x._id} product={x}/>)}
-                    </div>
+            <h2 className="text-center p-5">CUSTOM PRODUCTS</h2>
+            <div className="container mydiv">
+                <div className="row">
+                    {products.length > 0
+                        ? (<div className="row row-cols-3">
+                               {products.map(x => <ProductCardCustom key={x._id} product={x} />)}
+                        </div>)
+                        : <h3 className="text-center p-5">No products created yet</h3>
+                    }
                 </div>
-            </section>
+            </div>
+        </section>
 
     )
 }
