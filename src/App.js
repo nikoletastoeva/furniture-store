635c2a91e193b1180df1b22e8d944a92a0023be5
Nikoleta Stoeva <nikoletastoeva@abv.bg>
2021-12-21 17:08:57 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import Header from "./components/Header/Header";
-import { Route, Router } from 'react-router-dom'
+import { Redirect, Route, Router, Switch } from 'react-router-dom'
 import About from "./components/AboutAs/AboutAs";
 import Footer from "./components/Footer/Footer";
 import Home1 from "./components/Home/Home";
@@ -10,50 +10,43 @@ import Create from "./components/Create/Create";
 import Details from "./components/Details/Details";
 import CustomProducts from "./components/CustomProducts/CustomProducts";
 import Edit from "./components/Edit/Edit";
-import { AuthProvider } from "./contexts/AuthContext";
+import { AuthProvider, useAuthContext } from "./contexts/AuthContext";
 import { GuardedRoute } from "./components/Common/GuardedRoute";
 import DetailsCustomProduct from "./components/DetailsCustomProduct/DetailsCustomProduct";
 import MyLove from "./components/MyLove/MyLove";
 import Logout from "./components/Logout";
+import Page404 from "./components/Page404/Page404";
 
 
-import React from "react";
-export const Context = React.createContext();
 
 
 
 function App() {
-
+  
+  let user = localStorage.getItem('user')
+  console.log(user.email)
+ 
   return (
     <>
       <AuthProvider>
-        
+
         <Header />
-
-
-        <Route path='/' exact component={Home1} />
-        <Route path='/about-as' exact component={About} />
-        <Route path='/register' exact component={Register} />
-        <Route path='/login' exact component={Login} />
-        <Route path='/logout' exact component={Logout} />
-
-        <Route path='/details/:productId' exact component={Details} />
-        <Route path='/custom-products' exact component={CustomProducts} />
-        <Route path='/details-custom-product/:productId' exact component={DetailsCustomProduct} />
-
-        <Route>
-
-          
-        <Route path='/create' exact component={Create} />
-        
-        <Route path='/my-love' exact component={MyLove} />
-        
-
-          <Route path="/edit/:productId" exact component={Edit} />
-        </Route>
-
+        <Switch>
+          <Route path='/' exact component={Home1} />
+          <Route path='/about-as' exact component={About} />
+          <Route path='/register' exact component={Register} />
+          <Route path='/login' exact component={Login} />
+          <Route path='/logout' exact component={Logout} />
+          <Route path='/details/:productId' exact component={Details} />
+          <Route path='/custom-products' exact component={CustomProducts} />
+          <Route path='/details-custom-product/:productId' exact component={DetailsCustomProduct} />
+          <Route path= '/create' render={() => (user.email ? (<Create/>): (<Redirect to='/login'/>))}/>
+          <Route path= '/my-love' render={() => (user.email ? (<MyLove/>): (<Redirect to='/login'/>))}/>
+          <Route path= '//edit/:productId' render={() => (user.email ? (<Edit/>): (<Redirect to='/login'/>))}/>
+          <Route component={Page404} />
+        </Switch>
         <Footer />
-        
+
       </AuthProvider>
     </>
   );
