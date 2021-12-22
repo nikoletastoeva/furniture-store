d77d51052667901b18b2175363b2d6d85084d3d3
Nikoleta Stoeva <nikoletastoeva@abv.bg>
2021-12-22 16:43:26 +0200

    Update GuardedRoute.js

--- src/components/Common/GuardedRoute.js
+++ src/components/Common/GuardedRoute.js
@@ -3,20 +3,10 @@ import { Redirect } from "react-router-dom";
 import { Route } from "react-router-dom/cjs/react-router-dom.min";
 import { useAuthContext } from "../../contexts/AuthContext";
 
-import {Redirect, Route } from 'react-router-dom';
-import { useAuthContext } from '../../contexts/AuthContext';
 
-export const GuardedRoute = ({children, ...rest}) => {
 export const GuardedRoute = ({component: Component, ...rest}) => {
     const { isAuthenticated } = useAuthContext();
     return (
-        <Route {...rest} render={({ location }) => {
-          return isAuthenticated
-            ? children
-            : <Redirect to='/login'/>
-        }} />
-      )
-}
         <Route {...rest}
         render={props => {
             if(isAuthenticated){
