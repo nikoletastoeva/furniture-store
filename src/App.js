c998539fbd2e8c1627752cb31de14c93831feaad
Nikoleta Stoeva <nikoletastoeva@abv.bg>
2021-12-22 16:38:19 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 
 import Header from "./components/Header/Header";
-import { Redirect, Route, Router, Switch } from 'react-router-dom'
+import { Route,Switch } from 'react-router-dom'
 import About from "./components/AboutAs/AboutAs";
 import Footer from "./components/Footer/Footer";
 import Home1 from "./components/Home/Home";
@@ -10,12 +10,12 @@ import Create from "./components/Create/Create";
 import Details from "./components/Details/Details";
 import CustomProducts from "./components/CustomProducts/CustomProducts";
 import Edit from "./components/Edit/Edit";
-import { AuthProvider, useAuthContext } from "./contexts/AuthContext";
-import { GuardedRoute } from "./components/Common/GuardedRoute";
+import { AuthProvider} from "./contexts/AuthContext";
 import DetailsCustomProduct from "./components/DetailsCustomProduct/DetailsCustomProduct";
 import MyLove from "./components/MyLove/MyLove";
 import Logout from "./components/Logout";
 import Page404 from "./components/Page404/Page404";
+import { GuardedRoute } from "./components/Comon/GuardedRoute";
 
 
 
@@ -23,8 +23,10 @@ import Page404 from "./components/Page404/Page404";
 
 function App() {
   
-  let user = localStorage.getItem('user')
-  console.log(user.email)
+  let userData = localStorage.getItem('user')
+  let user = JSON.parse(userData)
+
+  
  
   return (
     <>
@@ -40,9 +42,10 @@ function App() {
           <Route path='/details/:productId' exact component={Details} />
           <Route path='/custom-products' exact component={CustomProducts} />
           <Route path='/details-custom-product/:productId' exact component={DetailsCustomProduct} />
-          <Route path= '/create' render={() => (user.email ? (<Create/>): (<Redirect to='/login'/>))}/>
-          <Route path= '/my-love' render={() => (user.email ? (<MyLove/>): (<Redirect to='/login'/>))}/>
-          <Route path= '//edit/:productId' render={() => (user.email ? (<Edit/>): (<Redirect to='/login'/>))}/>
+          <GuardedRoute path= '/create' exact component={Create}/>         
+          <GuardedRoute path= '/my-love' exact component={MyLove}/>          
+          <GuardedRoute path= '/edit/:productId' exact component={Edit}/>
+          
           <Route component={Page404} />
         </Switch>
         <Footer />
